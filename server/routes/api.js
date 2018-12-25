51d090c0b349a6e5974ab11a11472a5dd4311f87
Alon Rab <alonrab@outlook.com>
2018-12-25 13:40:34 +0200

    api mainpage

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,17 +1,16 @@
 const express = require(`express`)
 const router = express.Router()
 const path = require(`path`)
-const dist = path.resolve(__dirname,`../dist`)
-const mainpageHTML =`../dist/mainpage/mainpage.html`
-const organizationHTML = `./dist/organization/org.html`
-const restaurantHTML = `.dist/restaurant/restaurant.html`
+const dist = path.resolve(__dirname,`../../dist`)
+console.log()
+const mainpageHTML =`./mainpage.html`
 
 router.get(`/sanity`,function(req,res){
     res.send(`It Works!`)
 })
 router.get(`/`,function(req,res){
-    console.log()
     res.sendFile(mainpageHTML, {root: dist})
 })
+
 
 module.exports = router
